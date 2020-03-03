7ea127c1907318043522f4d9a9746895635f37f0
hala <hala.sebbah@gmail.com>
2020-03-03 10:01:44 +0100

    newbranche2 commit

--- src/main/java/com/sqli/App.java
+++ src/main/java/com/sqli/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "trynna 1 2 3 " );
+        System.out.println( "trynna 4 5 6" );
 
     }
 }
